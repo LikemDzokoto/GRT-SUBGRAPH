4a401d1d204f21d522a22d2309858b2d6396ad2f
LikemDzokoto <likemdzokoto@gmail.com>
2023-07-15 14:01:08 +0000

    initial commit

---
+++ src/graph-token.ts
@@ -0,0 +1,7 @@
+import {BigInt} from "@graphprotocol/graph-ts"
+import  {Transfer} from "../generated/GraphToken/GraphToken"
+import {User} from "../generated/schema"
+
+export  function handleTransfer(event:Transfer):void{
+
+}
